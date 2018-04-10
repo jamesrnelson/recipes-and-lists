1dc20e53afb89d0b4c12b5599d3101517a86f05e
JNelson <nelson.jimmy@gmail.com>
2018-04-09 21:16:20 -0600

    created user

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+# Defines User model
+class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
+end
