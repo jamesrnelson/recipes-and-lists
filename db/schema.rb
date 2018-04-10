a6e6f16801f0750d19e8b630540cad503e6d4dea
JNelson <nelson.jimmy@gmail.com>
2018-04-09 21:59:02 -0600

    created recipe migration and model. working on creating a default factory bot image.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180410031255) do
+ActiveRecord::Schema.define(version: 20180410034954) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "title"
+    t.integer "prep_time"
+    t.integer "cook_time"
+    t.text "instructions"
+    t.string "photo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
--- spec/features/user_visits_recipes_index_sees_all_recipes_spec.rb
+++ spec/features/user_visits_recipes_index_sees_all_recipes_spec.rb
@@ -2,6 +2,15 @@ require 'rails_helper'
 
 describe 'User visits recipes index' do
   it 'should have a list of all recipes with pictures' do
-    
+    recipes = create_list(:recipe, 5)
+
+    visit recipes_path
+
+    expect(page).to have_content(recipes.first.title)
+    expect(page).to have_content(recipes.first.prep_time)
+    expect(page).to have_content(recipes.first.cook_time)
+    expect(page).to have_content(recipes.last.title)
+    expect(page).to have_content(recipes.last.prep_time)
+    expect(page).to have_content(recipes.last.cook_time)
   end
 end
