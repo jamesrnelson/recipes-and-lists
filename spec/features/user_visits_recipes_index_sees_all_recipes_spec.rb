d228a955c78e47861adcca7248b073c5b86f3474
JNelson <nelson.jimmy@gmail.com>
2018-04-09 21:36:49 -0600

    added factory bot

---
+++ spec/features/user_visits_recipes_index_sees_all_recipes_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+describe 'User visits recipes index' do
+  it 'should have a list of all recipes with pictures' do
+    
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -27,11 +27,14 @@ end
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
+
+require 'database_cleaner'
+DatabaseCleaner.strategy = :truncation
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
@@ -61,4 +64,11 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.before(:each) do
+    DatabaseCleaner.clean
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 end
