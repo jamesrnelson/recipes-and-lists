de2a19df2d5f06862a65fc8e88ece85ab016c203
JNelson <nelson.jimmy@gmail.com>
2018-04-25 11:39:35 -0600

    Admin can delete a recipe

--- app/controllers/admin/recipes_controller.rb
+++ app/controllers/admin/recipes_controller.rb
@@ -5,9 +5,9 @@ class Admin::RecipesController < Admin::BaseController
   end
 
   def update
-    @recipe = Recipe.find(params[:id])
-    if @recipe.update(recipe_params)
-      flash[:success] = "You have updated #{@recipe.title}"
+    recipe = Recipe.find(params[:id])
+    if recipe.update(recipe_params)
+      flash[:success] = "You have updated #{recipe.title}"
       redirect_to recipes_path
     else
       flash[:error] = 'You were unable to update the recipe'
@@ -16,7 +16,13 @@ class Admin::RecipesController < Admin::BaseController
   end
 
   def destroy
-
+    recipe = Recipe.find(params[:id])
+    if recipe.destroy
+      flash[:success] = "Recipe #{recipe.title} was successfully deleted"
+    else
+      flash[:error] = "Recipe #{recipe.title} was not deleted"
+    end
+    redirect_to recipes_path
   end
 
   private
