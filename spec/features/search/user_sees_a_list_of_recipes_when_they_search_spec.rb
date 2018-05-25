d237fe8122146fde295384242118a96c4e34456c
JNelson <nelson.jimmy@gmail.com>
2018-05-25 13:20:21 -0600

    Wrote feature test for searching recipes through the api

---
+++ spec/features/search/user_sees_a_list_of_recipes_when_they_search_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+describe 'Registered User' do
+  context 'Visits recipes index and fills in search form' do
+    let(:attrs) {
+      label: 'Yummy Chicken',
+      image: 'https://www.edamam.com/web-img/ba6/ba6f66d885e4d62a98055b088a5a85a3.jpg',
+      source: 'BBC Food',
+      url: 'http://www.bbcgoodfood.com/recipes/3645/',
+      total_time: '60',
+      yield: '4',
+      ingredientLines: [
+            "100.0ml soy sauce (Kikkoman is good)",
+            "4.0 tbsp smooth peanut butter",
+            "4 skinless chicken breasts fillets"
+        ]
+    }
+
+    subject { OutsideRecipe.new(attrs) }
+
+    it 'should show a list of recipes from Edamam\'s API' do
+      user = create(:user)
+
+      allow_any_instance_of(ApplicationController)
+        .to receive(:current_user).and_return(user)
+
+      visit recipes_path
+
+      fill_in 'Search', with: 'chicken'
+
+      click_on 'Submit'
+
+      expect(page).to have_content(subject.title)
+      expect(page).to have_content(subject.image)
+      expect(page).to have_content(subject.creator)
+      expect(page).to have_content(subject.instructions)
+      expect(page).to have_content(subject.total_time)
+      expect(page).to have_content(subject.servings)
+      expect(page).to have_content(subject.ingredients[0])
+      expect(page).to have_content(subject.ingredients[1])
+      expect(page).to have_content(subject.ingredients[2])
+    end
+  end
+end
