b83fc7daea465b1b50a3ca2ae3a53ccef8cce496
JNelson <nelson.jimmy@gmail.com>
2018-05-25 15:33:13 -0600

    Added /search to routes'

--- config/routes.rb
+++ config/routes.rb
@@ -24,4 +24,5 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
+  get '/search', to: 'search#index'
 end
