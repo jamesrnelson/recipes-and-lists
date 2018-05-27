caec64964d60774337b9fcd580387e13833e5b5e
JNelson <nelson.jimmy@gmail.com>
2018-05-27 11:13:12 -0600

    Wrote a test for OutsideRecipeService that checks that it exists and that verifies the incoming keys from the JSON file

--- app/models/outside_recipe_listing.rb
+++ app/models/outside_recipe_listing.rb
@@ -1,5 +1,12 @@
 class OutsideRecipeListing
   def initialize(query)
-
+    @query = query
   end
+
+  private
+    attr_reader :query
+
+    def service
+      @service = OutsideRecipeService.new(query)
+    end
 end
---
+++ spec/services/outside_recipe_service_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe OutsideRecipeService do
+  subject { OutsideRecipeService.new("chicken") }
+
+  it 'exists' do
+    expect(subject).to be_an OutsideRecipeService
+  end
+
+  context 'instance methods' do
+    context '#recipe_search' do
+      it 'returns an array of hashes with recipe data' do
+        raw_search = subject.recipe_search
+        raw_recipes = raw_search[:hits]
+        raw_recipe = raw_recipes[4]
+
+        expect(raw_station).to have_key :label
+        expect(raw_station).to have_key :image
+        expect(raw_station).to have_key :source
+        expect(raw_station).to have_key :url
+        expect(raw_station).to have_key :yield
+        expect(raw_station).to have_key :ingredientLines
+      end
+    end
+  end
+end
