ea6f9354e83cb457e2c83c53f99ee8d41e38154c
JNelson <nelson.jimmy@gmail.com>
2018-06-02 11:44:43 -0600

    Increased test coverage on edge cases

---
+++ spec/models/outside_recipe_info_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe OutsideRecipeInfo do
+  subject { OutsideRecipeInfo.new('759372') }
+  it 'exists' do
+    expect(subject).to be_an OutsideRecipeInfo
+  end
+
+  context 'instance methods' do
+    context '#recipe' do
+      it 'should return the info for a single recipe', vcr: true do
+        expect(subject).to respond_to :recipe
+        recipe = subject.recipe
+        expect(recipe).to be_an OutsideRecipe
+      end
+    end
+  end
+end
