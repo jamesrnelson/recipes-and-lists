ca7f76ceb36344b901cd194099391474f5c44799
JNelson <nelson.jimmy@gmail.com>
2018-06-03 12:27:10 -0600

    updated schema and fixtures

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180603000544) do
+ActiveRecord::Schema.define(version: 20180603134704) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,8 @@ ActiveRecord::Schema.define(version: 20180603000544) do
     t.string "outside_recipe_id"
     t.string "servings"
     t.string "outside_image"
+    t.string "source_name"
+    t.string "source_url"
     t.index ["creator_id"], name: "index_recipes_on_creator_id"
   end
 
