54e154475a5863057f9dd4e4825196cf916194d5
JNelson <nelson.jimmy@gmail.com>
2018-06-05 14:45:48 -0600

    update

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -1,5 +1,6 @@
 class PurchaseController < ApplicationController
   def create
     GroceryListPresenter.new(params[:id]).buy_ingredients_from_amazon
+    redirect_to 'https://www.amazon.com/afx/ingredients/landing'
   end
 end
