5bccd1aabc82ff22cd1d24b5ec95a3fa30f7d262
JNelson <nelson.jimmy@gmail.com>
2018-06-11 10:57:11 -0600

    reversed favorites ordering

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,7 +3,7 @@ class DashboardController < ApplicationController
   def index
     @user = current_user
     if current_user
-      @favorites = current_user.user_recipes.order("created_at DESC")
+      @favorites = current_user.user_recipes.order("created_at ASC")
     else
       flash[:error] = "Create an account in order to add recipes to your favorites."
       redirect_to recipes_path
