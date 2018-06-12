4a905b018ef5a47e3204a63047112aa74a44357e
JNelson <nelson.jimmy@gmail.com>
2018-06-12 14:01:54 -0600

    added password confirmation when creating an account. Added before action to auto downcase user's email. Added .downcase method on new session controller to ensure that user's input is downcased to match the fact that user's email is downcased in the database.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,6 +34,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :email, :password)
+    params.require(:user).permit(:username, :email.downcase, :password, :password_confirmation)
   end
 end
